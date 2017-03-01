c7b681b405e082018a204a60580fcedfda98ec72
Chris Kavanagh <ckava3@gmail.com>
2017-03-01 08:59:51 -0500

    Added get_cart_total func.

--- cart/views.py
+++ cart/views.py
@@ -12,17 +12,18 @@ def show_cart(request):
 	product_ids = cart.keys()
 	prod_ids = [int(i) for i in product_ids]
 	products = Product.objects.filter(pk__in=prod_ids)
+
 	for product in products:
 		cart[str(product.id)]['product'] = product
 		cart[str(product.id)]['price'] = Decimal(cart[str(product.id)]['price'])
-		cart[str(product.id)]['total_price'] = cart[str(product.id)]['price'] * \
-						   					   cart[str(product.id)]['quantity']
-
+		cart[str(product.id)]['total_price'] = cart[str(product.id)]['price'] * cart[str(product.id)]['quantity']
 		q = cart[str(product.id)]['quantity']
-		cart[str(product.id)]['update_quantity_form'] = ItemAddForm(initial={'quantity':q,
-																			 'update': True})														 																	 
+		cart[str(product.id)]['update_quantity_form'] = ItemAddForm(initial={'quantity':q,'update': True})
+
+	cart_total = get_cart_total(request, cart)																		 														 																	 
 	cart = cart.values()
-	cxt = {'cart': cart}
+	cxt = {'cart': cart, 'cart_total': cart_total}
+	print cxt
 	return render(request, 'cart/cart_detail.html', cxt)
 
 
@@ -31,6 +32,7 @@ def add_item(request, pk):
 	product = get_object_or_404(Product, pk=pk)
 	product_id = str(product.pk)
 	cart = request.session.get(settings.CART_SESSION_ID, {})
+
 	if request.method == 'POST':
 		form = ItemAddForm(request.POST)
 		if form.is_valid():
@@ -60,14 +62,20 @@ def remove_item(request, pk):
 	return redirect('cart:show_cart')
 
 
+def get_cart_total(request, cart):
+	'''gets cart total price.'''
+	return sum(Decimal(item['price']) * item['quantity'] for item in cart.values())
+
+
 def clear_cart(request):
+	'''removes all items from cart.'''
 	cart = request.session.get(settings.CART_SESSION_ID)
 	cart = {}
 	request.session[settings.CART_SESSION_ID] = cart
 	request.session.modified = True
 
 
-def save_cart(request, c):
+def save_cart(request, cart):
 	'''save user cart to session.'''
-	request.session[settings.CART_SESSION_ID] = c
+	request.session[settings.CART_SESSION_ID] = cart
 	request.session.modified = True
