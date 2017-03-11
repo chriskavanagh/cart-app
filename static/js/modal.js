438f84f8e4cc2f6dcbdd2c837bbc8bcc8577967c
Chris Kavanagh <ckava3@gmail.com>
2017-03-11 04:17:40 -0500

    Disabled Like Button After Click.

--- static/js/modal.js
+++ static/js/modal.js
@@ -9,11 +9,13 @@
 $(function(){
 
 	$('#likes').click(function(){
+		$(this).removeAttr("disabled");
 		var proid;
 		proid = $(this).attr("data-proid");
 		$.get('/shop/like_product/', {product_id: proid}, function(data){
 			$('#like_count').html(data);
-			$('#likes').hide();
+			//$('#likes').hide();
+			$('#likes').attr("disabled","disabled");
 
 		});
 		
